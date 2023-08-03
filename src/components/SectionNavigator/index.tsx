035bfcf29e76c456ed96b43a384b0f8671974e0a
indraantoor <indraantoor@gmail.com>
2023-08-04 00:24:46 +0530

    refac(section-navigator): to use tailwind
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/components/SectionNavigator/index.tsx
+++ src/components/SectionNavigator/index.tsx
@@ -12,39 +12,44 @@ const SectionNavigator = ({
   forward,
 }: ISectionNavigatorProps) => {
   return (
-    <div
-      style={{
-        padding: "10px 5px",
-        display: "flex",
-        gap: "20px",
-      }}
-    >
-      {back ? (
-        <div
-          style={{
-            cursor: "pointer",
-          }}
-        >
-          Arrow
+    <div className="px-5 py-5 flex gap-5">
+      {back && (
+        <div className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
         </div>
-      ) : null}
-      <div
-        style={{
-          maxWidth: "100%",
-          overflow: "auto",
-        }}
-      >
-        {content}
-      </div>
-      {forward ? (
-        <div
-          style={{
-            cursor: "pointer",
-          }}
-        >
-          Arrow
+      )}
+      <div className="max-w-full overflow-auto">{content}</div>
+      {forward && (
+        <div className="cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+            />
+          </svg>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
